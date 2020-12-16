cb1ca4d27f5fa8afd2008d16227ca0b035bcb1f5
Joshua Reibert <joshua.reibert@dlr.de>
2020-12-16 14:21:20 +0100

    Draw touch points as circles

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,14 @@
 #include <argh.h>
 #include <spdlog/spdlog.h>
 
-#include <libevdev/libevdev.h>
+#include <multitouch_device.hpp>
+#include <program.hpp>
+#include <shader.hpp>
+
+struct Vertex {
+  glm::vec2 pos;
+  glm::vec3 color;
+};
 
 int main(int argc, const char* argv[]) {
 #ifdef NDEBUG
@@ -19,39 +26,111 @@ int main(int argc, const char* argv[]) {
 
   auto cmdl = argh::parser(argc, argv);
 
-  std::string dev_path = "/dev/input/event0";
-  if (cmdl.pos_args().size() >= 2) {
-    dev_path = cmdl.pos_args().at(1);
-    spdlog::info("Using device '{}'", dev_path);
-  }
-
-  struct libevdev* dev = NULL;
-  int fd;
-  int rc = 1;
-
-  fd = open(cmdl.pos_args().at(1).c_str(), O_RDONLY | O_NONBLOCK);
-  rc = libevdev_new_from_fd(fd, &dev);
-  if (rc < 0) {
-    spdlog::error("Failed to init libevdev: {}", strerror(-rc));
+  if (cmdl[1].empty()) {
+    spdlog::error("Please pass a device path as positional parameter!");
     return EXIT_FAILURE;
   }
-  printf("Input device name: \"%s\"\n", libevdev_get_name(dev));
-  printf("Input device ID: bus %#x vendor %#x product %#x\n",
-         libevdev_get_id_bustype(dev), libevdev_get_id_vendor(dev),
-         libevdev_get_id_product(dev));
-  if (!libevdev_has_event_type(dev, EV_REL) ||
-      !libevdev_has_event_code(dev, EV_KEY, BTN_LEFT)) {
-    printf("This device does not look like a mouse\n");
+  std::string device_path = cmdl[1];
+  MultitouchDevice mt_device{device_path};
+
+  glfwSetErrorCallback([](int code, const char* desc) {
+    spdlog::error("GLFW Error {}: \"{}\"", code, desc);
+  });
+
+  if (!glfwInit()) {
     return EXIT_FAILURE;
   }
 
-  do {
-    struct input_event ev;
-    rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL, &ev);
-    if (rc == 0)
-      printf("Event: %s %s %d\n", libevdev_event_type_get_name(ev.type),
-             libevdev_event_code_get_name(ev.type, ev.code), ev.value);
-  } while (rc == 1 || rc == 0 || rc == -EAGAIN);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+  GLFWwindow* window;
+  auto monitor = glfwGetPrimaryMonitor();
+  spdlog::info("Using monitor \"{}\"", glfwGetMonitorName(monitor));
+  auto mode = glfwGetVideoMode(monitor);
+  glm::uvec2 resolution{mode->width, mode->height};
+  window = glfwCreateWindow(resolution.x, resolution.y, "touchviz", monitor,
+                            nullptr);
+  if (!window) {
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+
+  glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode,
+                                int action, int mods) {
+    if (action == GLFW_PRESS) {
+      if (key == GLFW_KEY_Q || key == GLFW_KEY_ESCAPE) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+      }
+    }
+  });
+
+  // init OpenGL
+  glfwMakeContextCurrent(window);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    spdlog::error("Failed to load OpenGL");
+    return EXIT_FAILURE;
+  }
+  glViewport(0, 0, resolution.x, resolution.y);
+
+  auto vert_shader = gl::load_shader_from_file("shaders/simple.vert");
+  auto frag_shader = gl::load_shader_from_file("shaders/circle.frag");
+  auto program = gl::create_program({vert_shader, frag_shader});
+
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * mt_device.num_slots(), nullptr,
+               GL_DYNAMIC_DRAW);
+
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (GLvoid*)offsetof(Vertex, pos));
+  glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (GLvoid*)offsetof(Vertex, color));
+  glEnableVertexAttribArray(1);
+
+  glUseProgram(program);
+  glPointSize(100.0f);
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+    mt_device.update();
+    auto touch_points = mt_device.touch_points();
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    if (!touch_points.empty()) {
+      std::vector<Vertex> vertices;
+      for (const auto& touch_point : touch_points) {
+        if (!touch_point->active()) {
+          continue;
+        }
+        auto pos = touch_point->pos();
+        vertices.emplace_back(Vertex{pos, glm::vec3{1.0f}});
+      }
+      glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(Vertex) * vertices.size(),
+                      vertices.data());
+
+      glBindVertexArray(vao);
+      glDrawArrays(GL_POINTS, 0, vertices.size());
+    }
+    glfwSwapBuffers(window);
+  }
+
+  // cleanup
+  glDeleteVertexArrays(1, &vao);
+  glDeleteBuffers(1, &vbo);
+  glDeleteProgram(program);
+
+  glfwTerminate();
   return EXIT_SUCCESS;
 }
